d25f2e63a2f4eb8d8f8a7318f063613d86941b84
danwritecode <dnelson4993@outlook.com>
2023-08-20 20:10:58 -0400

    updating cta button on landing

--- pages/index.vue
+++ pages/index.vue
@@ -20,7 +20,7 @@
         <h1 class="mt-14 text-4xl font-bold tracking-tight text-zinc-100 sm:text-6xl">Freeing engineers from the overhead of Agile</h1>
         <p class="mt-6 text-lg leading-8 text-zinc-400">Our goal is to eliminate the overhead of agile. We are automating scrum, status updates, and sprint planning. Your engineers should spend time coding, not being micromanaged.</p>
         <div class="mt-10 flex items-center gap-x-6">
-          <NuxtLink to="/signup" class="rounded-md bg-sky-600 px-7 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-sky-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-sky-400 transition-hover">Get started</NuxtLink>
+          <NuxtLink to="/signup" class="rounded-md bg-sky-600 px-7 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-sky-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-sky-400 transition-hover">Signup for private beta</NuxtLink>
         </div>
       </div>
       <div class="mx-auto mt-16 flex max-w-2xl sm:mt-24 lg:ml-10 lg:mr-0 lg:mt-0 lg:max-w-none lg:flex-none xl:ml-32">
